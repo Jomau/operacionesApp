032a5790fc3589ab29c0ef53d4fb6a0f40413432
jomau05 <jomau05@gmail.com>
2022-05-05 20:21:28 -0500

    [Red]-[Sum] La clase Sum no existe

---
+++ src/test/OperationsTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OperationsTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void sumTest() {
+		Sum s = new Sum();
+	}
+
+}
