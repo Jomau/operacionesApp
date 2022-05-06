acf255d1592c85f650cd58334aba4f5147b6181d
jomau05 <jomau05@gmail.com>
2022-05-05 21:11:14 -0500

    [Green]-[Multiplication.calculate] Implementación del metodo calculate

--- src/operacionesApp/Multiplication.java
+++ src/operacionesApp/Multiplication.java
@@ -2,10 +2,14 @@ package operacionesApp;
 
 public class Multiplication extends Operations {
 
+	public Multiplication(int factor1, int factor2) {
+		this.term1 = factor1;
+		this.term2 = factor2;
+	}
+
 	@Override
 	public int calculate() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.term1 * this.term2;
 	}
 
 }
