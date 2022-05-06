d432c9e3c4b9712a3173d0db516379bcfd8bc663
jomau05 <jomau05@gmail.com>
2022-05-05 20:23:50 -0500

    [Green]-[Sum] La clase Sum fue creada

--- src/operacionesApp/test/OperationsTest.java
+++ src/operacionesApp/test/OperationsTest.java
@@ -1,9 +1,11 @@
-package test;
+package operacionesApp.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import operacionesApp.Sum;
 
 class OperationsTest {
 
